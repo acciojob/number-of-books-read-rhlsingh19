88886a0629d7630462eee6f9f9cc06cc52864d50
rhlsingh19 <145037560+rhlsingh19@users.noreply.github.com>
2024-01-04 21:32:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
-];
-
 function numberOfBooksRead(library) {
     let count = 0;
 
@@ -26,6 +8,29 @@ function numberOfBooksRead(library) {
     }
 
     return count;
+}
+
+// Example usage:
+const library = [
+    {
+        author: 'Author 1',
+        title: 'Book 1',
+        readingStatus: true
+    },
+    {
+        author: 'Author 2',
+        title: 'Book 2',
+        readingStatus: false
+    },
+    {
+        author: 'Author 3',
+        title: 'Book 3',
+        readingStatus: true
+    }
+];
+
+console.log(numberOfBooksRead(library)); // Output: 2
+
 
 
 // Do not change the code below
